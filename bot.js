2bc311a44748992001fb685701db5b509b2db34f
OmarNinga <47434394+OmarNinga@users.noreply.github.com>
2019-08-07 23:31:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "") return;
+  if (message.author.id !== "608457075311247374") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
